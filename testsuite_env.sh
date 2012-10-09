dbba3ebc1bdc9afa58b3a3d6b1bcf9577c03389f
bertocco <sara.bertocco@pd.infn.it>
2012-10-09 15:28:08 +0200

    Installation home fixed

--- testsuite_env.sh
+++ testsuite_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CREAM_TESTSUITE_HOME=$HOME/cream_blah_testsuites
+export CREAM_TESTSUITE_HOME=/opt/cream_blah_testsuites/cream_blah_testsuites
 
 export PYTHONPATH=$CREAM_TESTSUITE_HOME/lib:$CREAM_TESTSUITE_HOME/lib/conf:$CREAM_TESTSUITE_HOME/lib/utils:/opt/cream_test/lib
 
